fed49b1ee56b69a5e7aa559f0aae8e2552a41a27
reloadoskar <reloadoskar@gmail.com>
2019-12-03 11:07:11 -0600

    Actualizado mongodb, package.json y ip para conectar al servidor de google

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict'
-
+const mongodb = require('mongodb');
 var mongoose = require('mongoose');
 var app = require('./app');
 var port = 8080;
 
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/api_rest_hadria', {useNewUrlParser: true, useUnifiedTopology: true})
+// mongoose.connect('mongodb://localhost:27017/api_rest_hadria', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://34.70.127.109:27017/api_rest_hadria', {useNewUrlParser: true, useUnifiedTopology: true})
     .then( () => {
         console.log('Conectado a la base de datos HOMIE.');
 
