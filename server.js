a9e24da48212fa4976ea184b8df54061e6a11181
reloadoskar <reloadoskar@gmail.com>
2020-01-11 23:51:59 -0600

    cambio de server y puerto

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
  var app = require('./app_client');
 var mongoose = require('mongoose')
-var port = 8080;
+var port = 3000;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/hadria_COPIA_1', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://reloadoskar:MuffinTop100685@hdra1-qllhk.mongodb.net/DB_HADRIA2_ANGY?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+// mongoose.connect('mongodb://127.0.0.1:27017/hadria_COPIA_1', {useNewUrlParser: true, useUnifiedTopology: true})
 // mongoose.connect('mongodb://34.70.127.109:27017/hadria_angy', {useNewUrlParser: true, useUnifiedTopology: true})
     .then( () => {
         // Crear servidor
