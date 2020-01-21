ed991c8aae3511be7c2456bfa3cd1cda1074cf6c
reloadoskar <reloadoskar@gmail.com>
2020-01-21 10:07:27 -0600

    actualizado server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
- var app = require('./app_client');
+require('dotenv').config()
+var app = require('./app_client');
 var mongoose = require('mongoose')
 const PORT = process.env.PORT || 8000
-
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://reloadoskar:MuffinTop100685@hdra1-qllhk.mongodb.net/DB_HADRIA2_ANGY?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.mongoose_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 
     .then( () => {
         // Crear servidor
