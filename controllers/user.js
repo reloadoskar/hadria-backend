e700432e7d746b24e538aa6f69207f845865b409
reloadoskar <reloadoskar@gmail.com>
2020-07-23 16:27:34 -0500

    user.js controller cleaned

--- controllers/user.js
+++ controllers/user.js
@@ -74,40 +74,11 @@ var controller = {
                     let token = jwt.sign(payload, process.env.SECRET_KEY, {
                         expiresIn: 1440
                     })
-
-                    var db_client = "mongodb://127.0.0.1:27017/"+user.database
-                    var port = 8080;
-                    const options = {
-                        useNewUrlParser: true,
-                        autoIndex: false, // Don't build indexes
-                        reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-                        reconnectInterval: 500, // Reconnect every 500ms
-                        poolSize: 4, // Maintain up to 10 socket connections
-                        // If not connected, return errors immediately rather than waiting for reconnect
-                        bufferMaxEntries: 0,
-                        connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
-                        socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-                        family: 4 // Use IPv4, skip trying IPv6
-                      };
-                    // const conn = mongoose.createConnection(db_client, options);
-
-                    // const conn = mongoose.createConnection(db_client, {useNewUrlParser: true, useUnifiedTopology: true})
                     return res.status(200).send({
                         status: 'success',
                         message: 'Bienvenido '+payload.nombre,
                         token
                     })
-                    // .then( () => {
-                    //     console.log('WELCOME AGAIN!');
-                    //     return res.status(200).send({
-                    //         status: 'success',
-                    //         message: 'Bienvenido '+payload.nombre,
-                    //         token
-                    //     })
-                    // })
-                    // .catch(err => {
-                    //     console.log(err)
-                    // })
 
                 }else{
                     return res.status(200).send({
