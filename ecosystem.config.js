abe5d49f6d1967fd4ba2e00a76ade6231bebc648
reloadoskar <reloadoskar@gmail.com>
2021-02-22 13:52:47 -0600

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,16 @@
-export const apps = [{
-  name: "HADRIA2",
-  script: 'server_multi.js',
-  instances: "max",
-  env: {
-    NODE_ENV: "development",
-  },
-  env_production: {
-    NODE_ENV: "production",
-  },
-  exec_mode: "cluster",
-  max_memory_restart: "90M"
-}];
+module.exports = {
+  apps = [{
+    name: "HADRIA2",
+    script: 'server_multi.js',
+    instances: "max",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    },
+    exec_mode: "cluster",
+    max_memory_restart: "90M"
+  }]
+} 
+  
