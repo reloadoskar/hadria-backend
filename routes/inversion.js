3166c8e94a44d0b40d20ff925545d17d53622387
reloadoskar <reloadoskar@gmail.com>
2022-01-07 17:13:12 -0600

    update

--- routes/inversion.js
+++ routes/inversion.js
@@ -7,12 +7,13 @@ var router = express.Router();
 
 //Rutas
 router.post('/:bd/inversion/save', InversionController.save);
-router.get('/:bd/inversions/:mes', InversionController.getInversions);
+router.get('/:bd/inversions/:mes/:year', InversionController.getInversions);
 router.get('/:bd/inversion/:id', InversionController.getInversion);
 router.put('/:bd/close/inversion/:id', InversionController.close);
 router.put('/:bd/open/inversion/:id', InversionController.open);
 router.put('/:bd/update/inversion/:id', InversionController.update);
 router.delete('/:bd/inversion/delete/:id', InversionController.delete);
 router.put('/:bd/inversion/cancel/:id', InversionController.cancel);
+router.put('/:bd/inversion/update/', InversionController.update);
 
 module.exports = router;
